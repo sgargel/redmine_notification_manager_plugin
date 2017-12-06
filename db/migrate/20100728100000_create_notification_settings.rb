0aff77ad26e8d1807247d023f778ce0dd678cf5d
sgargel <sgargel@yahoo.it>
2017-12-06 11:50:07 +0100

    Subproject inherit settings (#1)
    * ugly inheritance by mysql trigger
    
    * improved comments
    
    * fix version

--- db/migrate/20100728100000_create_notification_settings.rb
+++ db/migrate/20100728100000_create_notification_settings.rb
@@ -10,9 +10,25 @@ class CreateNotificationSettings < ActiveRecord::Migration
       t.column :field, :string, :null => false
 
     end
+    # This is a ugly way to achieve inheritance goal using mysql triggers
+    # USE IN PRODUCTION AT YOUR OWN RISK
+    execute <<-_SQL
+    CREATE DEFINER=`redmine_default`@`localhost` TRIGGER `trg_upd_subproject_inherit_notification_settings` AFTER UPDATE ON `projects` FOR EACH ROW BEGIN
+    INSERT INTO notification_settings
+    select null,NEW.id,tracker_id,field
+    From notification_settings
+    where project_id = NEW.parent_id
+    AND OLD.id NOT IN (SELECT DISTINCT project_id from notification_settings);
+    END
+    _SQL
   end
 
   def self.down
     drop_table :notification_settings
+
+    # DROP the ugly trigger
+    execute <<-SQL
+      DROP TRIGGER `trg_upd_subproject_inherit_notification_settings`;
+    SQL
   end
 end
